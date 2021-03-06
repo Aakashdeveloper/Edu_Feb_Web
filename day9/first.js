97c9f3598a6c3f3f24787130165ae39962a9eba0
aakashdeveloper <ahanda205@gmail.com>
2021-03-06 22:14:04 +0530

    l

---
+++ day9/first.js
@@ -0,0 +1,13 @@
+$('h1')
+m.fn.init [h1, prevObject: m.fn.init(1), context: document, selector: "h1"]
+$('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+$('.scroll')
+m.fn.init(5) [a.scroll, a.scroll, a.scroll, a.scroll, a.scroll, prevObject: m.fn.init(1), context: document, selector: ".scroll"]
+$('.scroll')[0]
+<a href=​"#skills" class=​"scroll">​Skills​</a>​
+$('.scroll')[0].innerText
+"Skills"
+$('.scroll')[0].innerText="Tech"
+"Tech"
+
